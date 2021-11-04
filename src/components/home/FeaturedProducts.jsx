521145e51d330f02b1d69fc2b438c62abaaff880
Lamees Abou Darwish <lindas.dev.ic@gmail.com>
2021-11-04 11:56:30 +0200

    featured product Component part 2

--- src/components/home/FeaturedProducts.jsx
+++ src/components/home/FeaturedProducts.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Card } from 'react-bootstrap'
 
 class FeaturedProducts extends Component {
     render() {
@@ -11,8 +11,60 @@ class FeaturedProducts extends Component {
                         <p>Some of Our Exclusive Collection, You May Like</p>
                     </div>
                     <Row>
-                        <Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center" src="https://rukminim1.flixcart.com/image/416/416/kqjtd3k0/mobile/o/4/x/c11-2021-rmx3231-realme-original-imag4j4yrdt7d9cj.jpeg?q=70" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Realme C11 (Cool Grey, 32 GB)</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
 
+                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center" src="https://rukminim1.flixcart.com/image/416/416/ksnjp8w0/mobile/h/u/u/c21y-rmx3261-realme-original-imag65kcafgjqknz.jpeg?q=70" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Realme C11 (Cool Grey, 32 GB)</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center" src="https://rukminim1.flixcart.com/image/416/416/kqjtd3k0/mobile/s/b/8/c11-2021-rmx3231-realme-original-imag4j4xkqchhfxk.jpeg?q=70" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Realme C11 (Cool Grey, 32 GB)</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center" src="https://rukminim1.flixcart.com/image/416/416/ktx9si80/mobile/q/a/c/narzo-50a-rmx3430-realme-original-imag75kybaer8scz.jpeg?q=70" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Realme C11 (Cool Grey, 32 GB)</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center" src="https://rukminim1.flixcart.com/image/416/416/kklhbbk0/mobile/h/i/q/m3-mzb087ain-poco-original-imafzxf8zqkcgwfv.jpeg?q=70" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Realme C11 (Cool Grey, 32 GB)</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                        <Col className="p-1" key={1} xl={2} lg={2} md={2} sm={4} xs={6}>
+                            <Card className="image-box card">
+                                <img className="center" src="https://rukminim1.flixcart.com/image/416/416/ktx9si80/mobile/d/n/p/narzo-50a-rmx3430-realme-original-imag75kymngzyabx.jpeg?q=70" />
+                                <Card.Body>
+                                    <p className="product-name-on-card">Realme C11 (Cool Grey, 32 GB)</p>
+                                    <p className="product-price-on-card">Price : $120</p>
+                                </Card.Body>
+                            </Card>
                         </Col>
                     </Row>
 
