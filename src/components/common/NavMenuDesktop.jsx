3afc16450e05b5bb7659f008cdfdf2cf34370bd0
Fadi Zahhar <79freelancer@gmail.com>
2021-11-06 12:21:43 +0200

    adding the cards style and the bars images in the NavMenuDesktop

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -3,7 +3,7 @@ import {Navbar,Container, Row, Col,Button} from 'react-bootstrap';
 import Logo from '../../assets/images/easyshop.png';
 import {Link} from "react-router-dom";
 import MegaMenuAll from '../home/MegaMenu';
- 
+import Bars from '../../assets/images/bars.png';
    
  class NavMenuDesktop extends Component {
 
@@ -47,7 +47,7 @@ import MegaMenuAll from '../home/MegaMenu';
     <Container fluid={"true"} className="fixed-top shadow-sm p-2 mb-0 bg-white">
          <Row>
               <Col lg={4} md={4} sm={12} xs={12}>
-
+              <img onClick={this.MenuBarClickHandler} className="bar-img" src={Bars} />
               <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i>  </Button>
 
               <Link to="/"> <img className="nav-logo" src={Logo} /> </Link>
