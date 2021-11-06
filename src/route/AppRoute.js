2785c38c1cf044d783bc0dba5d8e2542cd421d3d
Fadi Zahhar <79freelancer@gmail.com>
2021-11-06 11:00:07 +0200

    adding the ProductDetilas Page in the AppRoute

--- src/route/AppRoute.js
+++ src/route/AppRoute.js
@@ -1,12 +1,15 @@
 import React, { Component, Fragment } from 'react'
 import { Router, Route, Switch } from 'react-router'
 import HomePage from '../pages/HomePage'
+import ProductDetailsPage from '../pages/ProductDetailsPage';
+
 class AppRoute extends Component {
     render() {
         return (
             <Fragment>
                 <Switch>
                     <Route exact to="/" component={HomePage} />
+                    <Route exact path="/productdetails" component={ProductDetailsPage} />
                 </Switch>
             </Fragment>
         )
