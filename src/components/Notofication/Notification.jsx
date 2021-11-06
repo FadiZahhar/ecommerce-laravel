a5cce635e90f7d1ae446b4510e206a75338e524c
Fadi Zahhar <79freelancer@gmail.com>
2021-11-06 11:27:30 +0200

    adding the basic required basic libraries from react and react bootstrap and exporting the class component to be used

---
+++ src/components/Notofication/Notification.jsx
@@ -0,0 +1,8 @@
+import React, { Component, Fragment } from 'react'
+import { Container,Row,Col, Card,Button,Modal } from 'react-bootstrap'
+
+class Notification extends Component {
+
+}
+
+export default Notification
