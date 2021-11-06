57092fb6b49024160139da24342dc1b76b9fce99
Fadi Zahhar <79freelancer@gmail.com>
2021-11-06 11:34:19 +0200

    adding the Link in the commone nav menu desktop and creating a notification page in the page folder

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router';
 
 class NavMenuDesktop extends Component {
     render() {
         return (
             <div>
-
+                <Link to="/notification" className="btn"><i className="fa h4 fa-bell"></i><sup><span className="badge text-white bg-danger">5</span></sup>                  
+                </Link>
             </div>
         )
     }
