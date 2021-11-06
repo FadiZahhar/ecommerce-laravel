54cf17d1ff8a73e59d98b5de6289cf8e9c6fe703
Fadi Zahhar <79freelancer@gmail.com>
2021-11-06 11:30:01 +0200

    adding the constructor to set the state of the component and both method of handle close and handle show that will change the state of the notification module fro show=true and show=false

--- src/components/Notofication/Notification.jsx
+++ src/components/Notofication/Notification.jsx
@@ -3,6 +3,21 @@ import { Container,Row,Col, Card,Button,Modal } from 'react-bootstrap'
 
 class Notification extends Component {
 
+    constructor(){
+        super();
+        this.state={
+             show:false,
+        }
+   }
+
+    handleClose = () =>{
+        this.setState({ show:false})
+    };  
+
+    handleShow = () => {
+         this.setState({ show:true })
+    }; 
+
 }
 
 export default Notification
