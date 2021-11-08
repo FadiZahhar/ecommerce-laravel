9e6d37d715488698e6fa20182ef5dc191ab8ec5a
MarieThereseMelhem <marietherese.melhem@hotmail.com>
2021-11-09 00:19:50 +0200

    Mobile View Menu Part 1

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -9,35 +9,37 @@ class NavMenuDesktop extends Component {
             <Fragment>
                 <div className="TopSectionDown">
                     <Navbar className="navbar" fixed="top" bg="light">
-                        <Container fluid={true} className="fixed-top shadow-sm p-2 mb-0 bg-white">
-                            <Row className="w-100">
-                                <Col lg={4} md={4} sm={12} xs={12}>
-                                    <Link to="/"><img className="nav-logo" src={Logo} /></Link>
-                                </Col>
+                        <Container fluid={true}>
+                            <Container fluid={true} className="fixed-top shadow-sm p-2 mb-0 bg-white">
+                                <Row>
+                                    <Col lg={4} md={4} sm={12} xs={12}>
+                                        <Link to="/"><img className="nav-logo" src={Logo} /></Link>
+                                    </Col>
 
-                                <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
-                                    <div className="input-group w-100">
-                                        <input type="text" className="form-control" />
-                                        <Button type="button" className="btn site-btn">
-                                            <i className="fa fa-search"></i>
-                                        </Button>
-                                    </div>
-                                </Col>
+                                    <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                                        <div className="input-group w-100">
+                                            <input type="text" className="form-control" />
+                                            <Button type="button" className="btn site-btn">
+                                                <i className="fa fa-search"></i>
+                                            </Button>
+                                        </div>
+                                    </Col>
 
-                                <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
-                                    <Link to="/" className="btn">
-                                        <i className="fa h4 fa-bell"></i>
-                                        <sup>
-                                            <span className="badge text-white bg-danger">
-                                                5
-                                            </span>
-                                        </sup>
-                                    </Link>
-                                    <a className="btn"><i className="fa h4 fa-mobile-alt"></i></a>
-                                    <Link to="/" className="h4 btn">LOGIN</Link>
-                                    <Button className="cart-btn"><i className="fa fa-shopping-cart"></i>  3 Items</Button>
-                                </Col>
-                            </Row>
+                                    <Col className="p-1 mt-1" lg={4} md={4} sm={12} xs={12}>
+                                        <Link to="/" className="btn">
+                                            <i className="fa h4 fa-bell"></i>
+                                            <sup>
+                                                <span className="badge text-white bg-danger">
+                                                    5
+                                                </span>
+                                            </sup>
+                                        </Link>
+                                        <a className="btn"><i className="fa h4 fa-mobile-alt"></i></a>
+                                        <Link to="/" className="h4 btn">LOGIN</Link>
+                                        <Button className="cart-btn"><i className="fa fa-shopping-cart"></i>  3 Items</Button>
+                                    </Col>
+                                </Row>
+                            </Container>
                         </Container>
                     </Navbar>
                 </div>
