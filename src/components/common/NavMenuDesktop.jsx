b0261ade471d53bbcf4f2f27c6cf8fa252214271
MarieThereseMelhem <marietherese.melhem@hotmail.com>
2021-11-09 19:14:46 +0200

    Mobile View Menu Part 5

--- src/components/common/NavMenuDesktop.jsx
+++ src/components/common/NavMenuDesktop.jsx
@@ -2,8 +2,37 @@ import React, { Component, Fragment } from 'react'
 import {Navbar, Container, Row, Col, Button} from 'react-bootstrap';
 import Logo from '../../assets/images/easyshop.png';
 import {Link} from 'react-router-dom';
+import MegaMenuAll from '../home/MegaMenuAll';
 
 class NavMenuDesktop extends Component {
+
+    constructor(){
+        super();
+        this.state={
+            SideNavState: "sideNavClose",
+            ContentOverState: "ContentOverlayClose"
+        }
+    }
+
+    MenuBarClickHandler=()=>{
+        this.SideNavOpenClose();
+    }
+
+    ContentOverlayClickHandler=()=>{
+        this.SideNavOpenClose();
+    }
+
+    SideNavOpenClose=()=>{
+        let SideNavState = this.state.SideNavState;
+        let ContentOverState = this.state.ContentOverState;
+        if(SideNavState === "sideNavOpen"){
+            this.setState({SideNavState:"sideNavClose", ContentOverState:"ContentOverlayClose"})
+        }
+        else{
+            this.setState({SideNavState:"sideNavOpen", ContentOverState:"ContentOverlayOpen"})
+        }
+    }
+
     render() {
         return (
             <Fragment>
@@ -13,6 +42,7 @@ class NavMenuDesktop extends Component {
                             <Container fluid={true} className="fixed-top shadow-sm p-2 mb-0 bg-white">
                                 <Row>
                                     <Col lg={4} md={4} sm={12} xs={12}>
+                                    <Button onClick={this.MenuBarClickHandler} className="btn"><i className="fa fa-bars"></i></Button>
                                         <Link to="/"><img className="nav-logo" src={Logo} /></Link>
                                     </Col>
 
@@ -41,6 +71,15 @@ class NavMenuDesktop extends Component {
                                 </Row>
                             </Container>
                         </Container>
+
+                        <div className={this.state.SideNavState}>
+                            <MegaMenuAll />
+                        </div>
+
+                        <div onClick={this.ContentOverlayClickHandler} className={this.state.ContentOverState}>
+
+                        </div>
+
                     </Navbar>
                 </div>
             </Fragment>
